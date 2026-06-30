9b815aefb97a6b5a261dbe62c5e3aec809aec688
mohamedali3261 <mohamedali3261@gmail.com>
2026-06-30 08:17:38 +0300

    simplify ping.js for testing

--- api/ping.js
+++ api/ping.js
@@ -1,5 +1,3 @@
 module.exports = (req, res) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Content-Type", "application/json");
   res.status(200).json({ ok: true, message: "pong" });
 };
